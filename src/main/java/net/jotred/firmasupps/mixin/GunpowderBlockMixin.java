1d75d37339dcb23e5acbe17e4b918e36e5d1e25a
JotredDev <148350408+JotredDev@users.noreply.github.com>
2025-11-09 12:57:22 +0100

    Fixed tags and mixins

--- src/main/java/net/jotred/firmasupps/mixin/GunpowderBlockMixin.java
+++ src/main/java/net/jotred/firmasupps/mixin/GunpowderBlockMixin.java
@@ -1,18 +1,25 @@
 package net.jotred.firmasupps.mixin;
 
 import net.mehvahdjukaar.supplementaries.common.block.blocks.GunpowderBlock;
+import net.mehvahdjukaar.supplementaries.common.misc.explosion.GunpowderExplosion;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.dries007.tfc.common.blocks.DeadTorchBlock;
 import net.dries007.tfc.common.blocks.DeadWallTorchBlock;
+import net.dries007.tfc.util.events.StartFireEvent;
 
 /**
  * Mixin to change the behaviour of the GunpowderBlock class to avoid getting lit by {@link DeadTorchBlock} and {@link DeadWallTorchBlock}.
@@ -22,8 +29,8 @@ import net.dries007.tfc.common.blocks.DeadWallTorchBlock;
 @Mixin(GunpowderBlock.class)
 public abstract class GunpowderBlockMixin
 {
-    @Inject(method = "canLightMeOnFire(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true, remap = false)
-    private static void fixCanLightMeOnFire(BlockState state, BlockGetter level, BlockPos pos, CallbackInfoReturnable<Boolean> cir, Block b)
+    @Inject(method = "canBlockLightMeOnFire(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true, remap = false)
+    private static void fixCanBlockLightMeOnFire(BlockState state, BlockGetter level, BlockPos pos, CallbackInfoReturnable<Boolean> cir, Block b)
     {
         if (b instanceof DeadTorchBlock || b instanceof DeadWallTorchBlock)
         {
